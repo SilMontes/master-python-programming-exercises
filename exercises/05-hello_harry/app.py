9b4e5f19a2931f9e8635d0012b6aa850765b8cf6
SilMontes <Silmontes2001@gmail.com>
2021-03-25 06:15:06 +0000

    avance inicial

--- exercises/05-hello_harry/app.py
+++ exercises/05-hello_harry/app.py
@@ -1,9 +1,10 @@
 #Complete the function below to print the output per the example.
+name=input("What is your name? ")
 def hello_name(name):
 
-    return None
+    return "Hello, " + name + '!'
 
 
 
 #Invoke the function with your name as the function's argument. 
-print(hello_name())
+print(hello_name(name))
