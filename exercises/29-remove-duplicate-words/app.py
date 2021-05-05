3319cf722c6074001296a935efae3a926a39f8d0
SilMontes <Silmontes2001@gmail.com>
2021-05-05 04:57:01 +0000

    Avance

--- exercises/29-remove-duplicate-words/app.py
+++ exercises/29-remove-duplicate-words/app.py
@@ -1,6 +1,17 @@
-def removeDuplicateAndSortWords(inputValue):
-    newValueList=list(set(list(inputValue.split(' '))))
-    newValueList.sort()
-    print(newValueList)
-    print(' '.join(map(str, newValueList)))
-removeDuplicateAndSortWords(input())
+# def removeDuplicateAndSortWords(inputValue):
+#     newValueList=list(set(list(inputValue.split(' '))))
+#     newValueList.sort()
+#     print(newValueList)
+#     print(' '.join(map(str, newValueList)))
+#removeDuplicateAndSortWords(input())
+phrase = input("Type in: ")
+phrase_splited = phrase.split(' ')
+
+word_list = []
+for i in phrase_splited:
+    if i not in word_list:
+        word_list.append(i)
+    else:
+        continue
+word_list.sort()
+print((' ').join(word_list))
---
+++ exercises/30-divisable-binary/app.py
@@ -0,0 +1,5 @@
+def divisibleBy5(givenDigits):
+    givenDigits=list(givenDigits)
+    for item in givenDigits:
+        print(int(item))
+divisibleBy5('100,11,1010,1001')
